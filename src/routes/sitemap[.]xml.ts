33eeb43f86dcdb060e67bf6ee6ec6ae2889e1e04
Lim <779793115@qq.com>
2026-07-28 23:14:45 +0800

    Add Japanese and Korean blog indexes to sitemap

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -19,7 +19,7 @@ const STATIC_PATHS: {
   { path: '/game', availableLocales: ALL_UI_LOCALES },
   { path: '/scoring-calculator', availableLocales: ALL_UI_LOCALES },
   { path: '/achievements', availableLocales: ALL_UI_LOCALES },
-  { path: '/blog', availableLocales: LONG_FORM_LOCALES },
+  { path: '/blog', availableLocales: ALL_UI_LOCALES },
   { path: '/how-to-play', availableLocales: LONG_FORM_LOCALES },
   { path: '/modes', availableLocales: LONG_FORM_LOCALES },
   { path: '/attributes', availableLocales: LONG_FORM_LOCALES },
