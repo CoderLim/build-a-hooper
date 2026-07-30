e697d467d80757d55dc6784e1f988a1d8d69341f
Lim <779793115@qq.com>
2026-07-30 10:18:22 +0800

    Redirect the legacy game route to the homepage

--- src/routes/game.tsx
+++ src/routes/game.tsx
@@ -1,42 +1,8 @@
-import { createFileRoute } from '@tanstack/react-router';
-
-import { buildPageHead } from '@/lib/seo/metadata';
-import { m } from '@/paraglide/messages.js';
-import { getLocale, locales } from '@/paraglide/runtime.js';
-import { GameGuide } from '@/blocks/game-guide';
-import { Header } from '@/blocks/header';
-import { HooperGame } from '@/components/hooper-game/hooper-game';
-
-function GamePage() {
-  return (
-    <div className="bg-background text-foreground flex min-h-screen flex-col">
-      <Header />
-      <main className="flex flex-1 flex-col">
-        <HooperGame />
-        <GameGuide />
-      </main>
-    </div>
-  );
-}
+import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/game')({
-  loader: () => {
-    const locale = getLocale();
-    return {
-      locale,
-      title: m['game.meta.title']({}, { locale }),
-      description: m['game.meta.description']({}, { locale }),
-    };
+  beforeLoad: () => {
+    throw redirect({ to: '/', statusCode: 301 });
   },
-  head: ({ loaderData }) =>
-    loaderData
-      ? buildPageHead({
-          title: loaderData.title,
-          description: loaderData.description,
-          path: '/game',
-          locale: loaderData.locale,
-          alternateLocales: locales,
-        })
-      : {},
-  component: GamePage,
+  component: () => null,
 });
